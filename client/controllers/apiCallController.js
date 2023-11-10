fdf5678115829929962b681040728c3a4b63b4c7
LeoGrouet <leo.grouet@gmail.com>
2023-11-10 16:32:25 +0100

    Mise en place view type / test du fetch des tout les pokemons en fonction du type en cours

--- client/controllers/apiCallController.js
+++ client/controllers/apiCallController.js
@@ -1,12 +1,14 @@
 const datamapper = require("../dataMapper/datamapper");
-let limit = 18;
 
 const APIFetch = {
-  allPokemon: async function (req, res) {
+  allPokemon: async (req, res) => {
     try {
+      const limit = req.query.limit || 20; // Récupérez la limite depuis la requête (ou utilisez 20 par défaut)
+      const offset = req.query.offset || 0; // Récupérez l'offset depuis la requête (ou utilisez 0 par défaut)
       const pokemonsNameResponse = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=${limit}`
+        `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
       );
+
       const pokemons = await pokemonsNameResponse.json();
       const pokemonData = pokemons.results;
       // const types = pokemons.
@@ -30,20 +32,26 @@ const APIFetch = {
         pokemonImages.push(imageData);
         pokemonTypes.push(imageData.types);
       }
+
       res.render("home", {
         pokemon: pokemonData,
         pokeImg: pokemonImages,
         pokeType: pokemonTypes,
+        limit: limit, // Transmettez la limite à la vue
+        offset: offset, // Transmettez l'offset à la vue
       });
     } catch (error) {
       res.status(500).send(error.message);
     }
   },
 
-  onePokemonByName: async function (req, res) {
+  onePokemonByNumber: async (req, res) => {
     try {
-      const pokemonName = req.params.order; // Utilisez req.params pour obtenir le nom du Pokémon
-      const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+      const pokemonNumber = req.params.order; // Utilisez req.params pour obtenir le nom du Pokémon
+      console.log(pokemonNumber);
+      let url = `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`;
+      console.log(url);
+
       const apiResponse = await fetch(url);
       if (apiResponse.status !== 200) {
         res.status(apiResponse.status).send("Le Pokémon n'a pas été trouvé.");
@@ -52,7 +60,7 @@ const APIFetch = {
 
       const getPokemonUrl = await apiResponse.json();
       const pokemon = getPokemonUrl;
-      console.log(pokemon.order);
+      console.log(pokemon.id);
       const stats = pokemon.stats;
       const statTable = [];
       for (const stat of stats) {
@@ -63,6 +71,46 @@ const APIFetch = {
         pokemon,
         statTable,
       });
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  },
+
+  getTypes: async (req, res) => {
+    try {
+      const urlTypes = await fetch(
+        `https://pokeapi.co/api/v2/type?limit=18&offset=0`
+      );
+      const typesJson = await urlTypes.json();
+      const types = typesJson.results;
+      res.render("types", {
+        types,
+      });
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  },
+
+  getPokemonsOfOneType: async (req, res) => {
+    try {
+      const typeToFind = req.params.type;
+      const fetching = await fetch(
+        `https://pokeapi.co/api/v2/type/${typeToFind}`
+      );
+      const fetchingPokemonsofThisType = await fetching.json();
+      const pokemonFetchs = fetchingPokemonsofThisType.pokemon;
+
+      const pokemons = [];
+      for (const pokemon of pokemonFetchs) {
+        pokemons.push(pokemon.pokemon);
+        const infosPokemon = await fetch(
+          `https://pokeapi.co/api/v2/type/${pokemon.pokemon.url}`
+        );
+        const info = await infosPokemon.name;
+        console.log(info);
+      }
+
+      res.render("home", {});
     } catch (error) {
       res.status(500).send(error.message);
     }
--- router.js
+++ router.js
@@ -4,6 +4,8 @@ const APIFetch = require("./client/controllers/apiCallController");
 const router = express.Router();
 
 router.get("/", APIFetch.allPokemon);
-router.get("/pokemon/:order", APIFetch.onePokemonByName);
+router.get("/pokemon/:order", APIFetch.onePokemonByNumber);
+router.get("/types", APIFetch.getTypes);
+router.get("/type/:type", APIFetch.getPokemonsOfOneType);
 
 module.exports = router;
