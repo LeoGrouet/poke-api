9cfa0e0f615bfd2701956c2ee2a2563dd02b7558
LeoGrouet <leo.grouet@gmail.com>
2024-02-01 22:50:02 +0100

    feat: image contrast back to normal if guess correct

--- client/js/guessThePokemon.js
+++ client/js/guessThePokemon.js
@@ -1,7 +1,7 @@
 function handleTheGuess() {
   const userGuess = document.getElementById("user-guess").value;
   const pokemonToGuess = document.getElementById("name-to-guess").innerText;
-
+  const pokeImage = document.querySelector(".pokemon-to-find");
   console.log(userGuess, pokemonToGuess);
 
   if (
@@ -9,6 +9,7 @@ function handleTheGuess() {
     userGuess.trim().toLocaleLowerCase()
   ) {
     console.log("GG mec");
+    pokeImage.style.filter = "contrast(1)";
   } else {
     console.log("T'es bourré ?");
   }
