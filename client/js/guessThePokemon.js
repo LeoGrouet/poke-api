ba0a872f51a161364fa74608618ec5e2b18822f3
LeoGrouet <leo.grouet@gmail.com>
2024-02-02 11:48:05 +0100

    feat: new div append after the guess

--- client/js/guessThePokemon.js
+++ client/js/guessThePokemon.js
@@ -2,6 +2,7 @@ function handleTheGuess() {
   const userGuess = document.getElementById("user-guess").value;
   const pokemonToGuess = document.getElementById("name-to-guess").innerText;
   const pokeImage = document.querySelector(".pokemon-to-find");
+  const pokeDiv = document.querySelector(".guess-pokemon-div");
   console.log(userGuess, pokemonToGuess);
 
   if (
@@ -10,7 +11,15 @@ function handleTheGuess() {
   ) {
     console.log("GG mec");
     pokeImage.style.filter = "contrast(1)";
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = "tu as gagné";
+    newDiv.classList.add = "winning-div";
+    pokeDiv.appendChild(newDiv);
   } else {
     console.log("T'es bourré ?");
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = "c'est perdu";
+    newDiv.classList.add = "loosing-div";
+    pokeDiv.appendChild(newDiv);
   }
 }
